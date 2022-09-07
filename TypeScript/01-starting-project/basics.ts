871a0f5e3695e02a78fe425f7cb3f08ac3feeda5
Grace You <graceyouya500@gmail.com>
2022-09-07 20:06:07 +1000

    start with typescript

---
+++ TypeScript/01-starting-project/basics.ts
@@ -0,0 +1,15 @@
+// Primitives: number, string, boolean, null, undefine
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives:
+
+let age: number = 23; // string and number type useing the lowercase, if Number => will point to Number object in js.
+age = 12.2;
+
+let userName: string;
+userName = "desegfh";
+userName = "12";
+
+let isInstructor: boolean;
+isInstructor = true;
